926f6009ae83ae329fe3a447efce3f9090ba9665
nico14916 <nico@minetex.net>
2021-02-06 20:51:58 -0500

    update knex

--- api/knex.js
+++ api/knex.js
@@ -1,13 +1,17 @@
-let connection = process.env.PG_CONNECTION_STRING;
+let connection = {
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+}
 if(process.env.PG_SSL_CA){
-    connection = {
-        connectionString: process.env.PG_CONNECTION_STRING,
-        ssl:{
-            ca: process.env.PG_SSL_CA.replace(/\\n/g, '\n')
-        }
+    connection.ssl = {
+        rejectUnauthorized: true,
+        ca: process.env.PG_SSL_CA.replace(/\\n/g, '\n')
     }
 }
-
+console.log(connection);
 module.exports = require('knex')({
     client: 'pg',
     connection: connection,
